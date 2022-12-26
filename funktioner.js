e517c77e48b956d7ae5929825220dac4a18da627
Christian Rodrigo <chri65mb@stud.kea.dk>
2022-12-26 17:27:52 +0100

    portfolie problem, overflow

--- funktioner.js
+++ funktioner.js
@@ -1,3 +1,5 @@
+/* Burger menu */
+
 document.addEventListener("DOMContentLoaded", nav);
 
 function nav() {
@@ -7,4 +9,38 @@ function nav() {
   burger.addEventListener("click", () => {
     nav.classList.toggle("show");
   });
+}
+
+/* portfolie popup */
+/* 02_web */
+let popupwindow1 = document.getElementById("web");
+function openPopUpWeb() {
+  popupwindow1.classList.add("open-popup");
+}
+function closePopupWeb() {
+  popupwindow1.classList.remove("open-popup");
+}
+/* 03_ux */
+let popupwindow2 = document.getElementById("ux");
+function openPopUpUx() {
+  popupwindow2.classList.add("open-popup");
+}
+function closePopupUx() {
+  popupwindow2.classList.remove("open-popup");
+}
+/* 04_animation */
+let popupwindow3 = document.getElementById("animation");
+function openPopUpAn() {
+  popupwindow3.classList.add("open-popup");
+}
+function closePopupAn() {
+  popupwindow3.classList.remove("open-popup");
+}
+/* 05_indhold */
+let popupwindow4 = document.getElementById("indhold");
+function openPopUpIn() {
+  popupwindow4.classList.add("open-popup");
+}
+function closePopupIn() {
+  popupwindow4.classList.remove("open-popup");
 }
